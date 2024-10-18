fece8f2c85ede2f574b7c40ec761b449ed8abb73
ckamilokoo <camilo.camposog@gmail.com>
2024-10-18 00:57:06 -0300

    nueva configuracion

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,7 +2,28 @@ from flask import Flask
 from flask_cors import CORS
 from app.rutas.rutas import main_bp
 from app.modelo.sql import db  # Importa tu base de datos desde el archivo correspondiente
-from app.config import ServerConfig
+
+import os 
+from dotenv import load_dotenv
+import base64
+
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+certificate_base64 = os.getenv("CERTIFICATE_BASE64")
+cert_bytes = base64.b64decode(certificate_base64)
+
+# Almacena el certificado en un archivo temporal
+with open("/tmp/certificate.crt", "wb") as cert_file:
+    cert_file.write(cert_bytes)
+
+
+class ServerConfig:
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def create_app():
     app = Flask(__name__)
