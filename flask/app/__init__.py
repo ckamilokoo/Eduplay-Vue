505de1591fc1375d38d78f75f281ff042861a18d
ckamilokoo <camilo.camposog@gmail.com>
2025-04-01 11:33:39 -0300

    cambios en la conexion a la base de datos

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -11,13 +11,6 @@ import base64
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-certificate_base64 = os.getenv("CERTIFICATE_BASE64")
-cert_bytes = base64.b64decode(certificate_base64)
-
-# Almacena el certificado en un archivo temporal
-with open("/tmp/certificate.crt", "wb") as cert_file:
-    cert_file.write(cert_bytes)
-
 
 class ServerConfig:
 
